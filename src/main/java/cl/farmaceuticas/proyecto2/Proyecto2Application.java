99fc98eb6dba7ac31c0fa9ae09d245236d4fdc2d
Joaco202 <joakocontreras06@gmail.com>
2025-05-05 22:52:52 -0400

    inicio proyecto

--- src/main/java/cl/farmaceuticas/proyecto2/Proyecto2Application.java
+++ src/main/java/cl/farmaceuticas/proyecto2/Proyecto2Application.java
@@ -8,6 +8,7 @@ public class Proyecto2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Proyecto2Application.class, args);
+		System.out.println("hola mundo");
 	}
 
 }
