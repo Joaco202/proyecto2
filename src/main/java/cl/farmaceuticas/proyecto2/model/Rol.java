55eef542d5b5ef21ee588136a4d9844194befe88
Joaco202 <joakocontreras06@gmail.com>
2025-05-10 01:28:59 -0400

    agregadas una dependencias e iniciando con Rol

--- src/main/java/cl/farmaceuticas/proyecto2/model/Rol.java
+++ src/main/java/cl/farmaceuticas/proyecto2/model/Rol.java
@@ -1,37 +1,39 @@
 package cl.farmaceuticas.proyecto2.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
 public class Rol {
-    private int id_rol;
-    private String nombre_rol;
-    private String descripcion_rol;
+   
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    public Rol(int id_rol, String nombre_rol, String descripcion_rol) {
-        this.id_rol = id_rol;
-        this.nombre_rol = nombre_rol;
-        this.descripcion_rol = descripcion_rol;
+    @Column(name = "nombre")
+    private String nombre;
+
+    public Rol() {
     }
 
-    public int getIdRol() {
-        return id_rol;
+    public Rol(Long id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
     }
 
-    public void setIdRol(int id_rol) {
-        this.id_rol = id_rol;
+    public Long getId() {
+        return id;
     }
 
-    public String getNombreRol() {
-        return nombre_rol;
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    public void setNombreRol(String nombre_rol) {
-        this.nombre_rol = nombre_rol;
+    public String getNombre() {
+        return nombre;
     }
 
-    public String getDescripcionRol() {
-        return descripcion_rol;
-    }
-
-    public void setDescripcionRol(String descripcion_rol) {
-        this.descripcion_rol = descripcion_rol;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 }
--- src/main/java/cl/farmaceuticas/proyecto2/repository/RolRepository.java
+++ src/main/java/cl/farmaceuticas/proyecto2/repository/RolRepository.java
@@ -1,5 +1,12 @@
 package cl.farmaceuticas.proyecto2.repository;
 
-public class RolRepository {
-    
+import cl.farmaceuticas.proyecto2.model.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RolRepository extends JpaRepository<Rol, Long>{
+
+	public Rol findByNombre(String nombre);
+	
 }
