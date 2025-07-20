196dbc2d21948d5a907357f0fad82d390e32ee4f
sebapedrerosm <sebastian.pedreros2001@alumnos.ubiobio.cl>
2025-07-20 02:46:55 -0400

    login

---
+++ src/main/java/cl/farmaceuticas/proyecto2/controller/LoginController.java
@@ -0,0 +1,17 @@
+package cl.farmaceuticas.proyecto2.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login"; // Debe haber un archivo login.html en src/main/resources/templates si usas Thymeleaf
+    }
+
+    @GetMapping("/menu")
+    public String home() {
+        return "home"; // Página después de loguearse
+    }
+}
