8e3128f2e7e4e54ace62d8b6051afd69fe778fdf
sebapedrerosm <sebastian.pedreros2001@alumnos.ubiobio.cl>
2025-07-19 21:15:58 -0400

    cambios modelo usuari0

--- src/main/java/cl/farmaceuticas/proyecto2/model/Usuario.java
+++ src/main/java/cl/farmaceuticas/proyecto2/model/Usuario.java
@@ -36,7 +36,7 @@ public class Usuario {
     private LocalDateTime fechaModificacion;
 
     @ManyToOne
-    @JoinColumn(name = "rol_id", insertable = false, updatable = false)
+    @JoinColumn(name = "rol_id")
     private Rol rol;
 
     @OneToMany(mappedBy = "usuario")
