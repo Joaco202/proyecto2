a6e6f981d1fd77881266fca375c5aeaccadb00cf
sebapedrerosm <sebastian.pedreros2001@alumnos.ubiobio.cl>
2025-07-19 20:19:07 -0400

    cambios authcontroller

--- src/main/java/cl/farmaceuticas/proyecto2/controller/AuthController.java
+++ src/main/java/cl/farmaceuticas/proyecto2/controller/AuthController.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
-public class AuthService {
+public class AuthController {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
